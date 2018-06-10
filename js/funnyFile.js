53999d24912cc5587b8583b0a6fc959119f87a79
mdm911 <mdm911@yahoo.com>
2018-06-10 01:09:44 -0500

    Added js file

---
+++ js/funnyFile.js
@@ -0,0 +1,10 @@
+function pickStory() {
+    let storyChoice = document.getElementById("story").value;
+    switch (storyChoice) {
+        case 1:
+            story1.style.display = 'inline-block';
+            break;
+
+    }
+}
+
